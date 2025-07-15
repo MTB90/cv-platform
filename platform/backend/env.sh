e59f321386bc7e50d317b255b04666de108c28c9
Mati <bojanowski.mateusz@gmail.com>
2025-07-15 02:00:03 +0200

    [Backend] Added alembic simple configuration

--- platform/backend/env.sh
+++ platform/backend/env.sh
@@ -1,6 +1,6 @@
 export CV_BACKEND_PROJECT_NAME=cv-platform-backend
 
-export CV_BACKEND_DB_HOST=localhost:54321/db
+export CV_BACKEND_DB_HOST=localhost:54321/backend-db
 export CV_BACKEND_DB_USER=user
 export CV_BACKEND_DB_PASS=pass
 
