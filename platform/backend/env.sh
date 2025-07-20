daf79557d34780de5d81501afcea2b4422882154
Mati <bojanowski.mateusz@gmail.com>
2025-07-20 18:32:01 +0200

    [GitOps] Extend docker file for testing.

--- platform/backend/env.sh
+++ platform/backend/env.sh
@@ -2,5 +2,7 @@ export CV_BACKEND_DB_HOST=localhost:54321
 export CV_BACKEND_DB_NAME=platform
 export CV_BACKEND_DB_USER=platform
 export CV_BACKEND_DB_PASS=password
+export CV_BACKEND_MINIO_ACCESS_KEY=minioAccessKey
+export CV_BACKEND_MINIO_SECRET_KEY=minioSecretKey
 
 env | grep CV_BACKEND_ > .env
